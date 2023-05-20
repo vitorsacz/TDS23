2e1d426fd324bd42b7bed3ade48584f4dbdde264
VITOR CRUZ DOS SANTOS <3258.343@edu.fb.org.br>
2023-05-19 21:52:35 -0300

    PRIMEIRO EXEMPLO DE COMANDOS SQL

---
+++ aula1.sql
@@ -0,0 +1,8 @@
+use AdventureWorks2012
+
+select *  from sys.tables
+
+select * from Person.person
+select FirstName from person.person
+select FirstName, LastName from person.person
+select FirstName as PrimeiroNome, LastName as UltimoNome from person.person
